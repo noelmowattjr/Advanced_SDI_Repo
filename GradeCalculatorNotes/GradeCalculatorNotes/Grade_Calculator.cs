25fd8363ebc52a3e36ce7066a808fcc274f33919
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-21 17:54:32 -0500

    Lecture notes - Grade Calculator
    Lecture notes - Grade Calculator

---
+++ GradeCalculatorNotes/GradeCalculatorNotes/Grade_Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculatorNotes
+{
+    class Grade_Calculator
+    {
+        // Hard coded values should be placed in
+        // variables at the top of the file they are
+        // used in for ease of modification to the program.
+        static int _NumAssignments = 16;
+        static int[] _AssignmentWeights =
+        {
+1, // Course info quiz
+10, // Professionalism
+5, // CE01
+6, // CE02
+6, // CE03
+5, // Quiz 1
+6, // CE04
+5, // CE05
+10, // CE06
+5, // Quiz 2
+7, // CE07
+7, // CE08
+7, // CE09
+5, // Quiz 3
+10, // Synth
+5 // Final
+};
+        // Declare an array member varialbe of type floats
+        public float[] _Grades;
+        public string _Name;
+        // Default constuctor
+        public Grade_Calculator()
+        {
+            // Instantiate an array of floats and assign it
+            // to the _Grades member variable.
+            _Grades = new float[_NumAssignments];
+        }
+        public Grade_Calculator(string Name)
+        {
+            // Showing the hidden this parameter
+            this._Name = Name;
+            _Grades = new float[_NumAssignments];
+        }
+        // Method signature
+        // (Access modifer) (return type) (Method Name) (Parameter(s))
+        public float GetFinalGrade()
+        ///////////////////////////////
+        {
+            float total = 0;
+
+            // loop through all of the grades and multiply
+            // by the assignment weight and total it up
+            // to get the final grade.
+            for (int i = 0; i < _NumAssignments; ++i)
+            {
+                total += (_Grades[i] * _AssignmentWeights[i]);
+            }
+            total /= 100;
+            return total;
+        }
+        // Overload method
+        public float GetFinalGrade(bool autoFail)
+        {
+            float total = 0;
+
+            return total;
+        }
+    }
+}
---
+++ GradeCalculatorNotes/GradeCalculatorNotes/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculatorNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Primitve type
+            // int, bool, char, short, double, float,
+            // string
+            int myNumber = 5;
+            string name = "Sue";
+            char oneCharacter = 'A';
+
+            // Reference type
+            // Class
+            Grade_Calculator gc;
+            gc = new Grade_Calculator();
+            if (gc == null)
+            {
+            }
+
+            // Declare a variable
+            Grade_Calculator Adam;
+
+            // Instantiate an object of type GradeCalculator
+            Adam = new Grade_Calculator("Adam");
+
+            // Declare a varialbe and instantiate an object
+            // of type GradeCalculator.
+            Grade_Calculator Bob = new Grade_Calculator("Bob");
+            Adam._Grades[0] = 100;
+            Adam._Grades[1] = 47;
+            Adam._Grades[2] = 82;
+            Adam._Grades[3] = 70;
+            Adam._Grades[4] = 70;
+            Adam._Grades[5] = 32;
+            Adam._Grades[6] = 60;
+            Adam._Grades[7] = 80;
+            Adam._Grades[8] = 90;
+            Adam._Grades[9] = 47;
+            Adam._Grades[10] = 50;
+            Adam._Grades[11] = 17;
+            Adam._Grades[12] = 23;
+            Adam._Grades[13] = 57;
+            Adam._Grades[14] = 42;
+            Adam._Grades[15] = 80;
+            Bob._Grades[0] = 100;
+            Bob._Grades[1] = 90;
+            Bob._Grades[2] = 82;
+            Bob._Grades[3] = 85;
+            Bob._Grades[4] = 69;
+            Bob._Grades[5] = 70;
+            Bob._Grades[6] = 90;
+            Bob._Grades[7] = 100;
+            Bob._Grades[8] = 90;
+            Bob._Grades[9] = 55;
+            Bob._Grades[10] = 50;
+            Bob._Grades[11] = 47;
+            Bob._Grades[12] = 85;
+            Bob._Grades[13] = 57;
+            Bob._Grades[14] = 90;
+            Bob._Grades[15] = 80;
+
+            // cw + tab x 2 fills in Console.WriteLine()
+            Console.WriteLine("{0}'s grade was: {1}", Adam._Name, Adam.GetFinalGrade());
+            Console.WriteLine("{0}'s grade was: {1}", Bob._Name, Bob.GetFinalGrade(true));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
