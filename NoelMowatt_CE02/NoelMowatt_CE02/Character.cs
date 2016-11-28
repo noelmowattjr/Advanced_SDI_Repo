5511ca91ad1454acbfc2727f5e0c1979c674190a
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-28 17:48:05 -0500

    Created: Constructors - getters & setters
    Built constructors for my Weapon and Character classes. Also, created
    getters and setters for each class member field.

--- NoelMowatt_CE02/NoelMowatt_CE02/Character.cs
+++ NoelMowatt_CE02/NoelMowatt_CE02/Character.cs
@@ -8,5 +8,59 @@ namespace NoelMowatt_CE02
 {
     class Character
     {
+        //--Class member fields
+        private string Name;
+        private int BaseAttack;
+        private int Health;
+        //--private Weapon Equipped;
+        private int Age;
+        private char Gender;
+        
+        //--Constructor--//
+        public Character(string name, int baseattack, int health, Weapon equipped, int age, char gender)
+        {
+            Name = name; BaseAttack = baseattack;
+            Health = health; //--Equipped = equipped;
+            Age = age; Gender = gender;
+        }
+
+        //-----------Getters & Setters-------------//
+        public string Name_GS
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public int BaseAttack_GS
+        {
+            get { return BaseAttack; }
+            set { BaseAttack = value; }
+        }
+
+        public int Health_GS
+        {
+            get { return Health; }
+            set { Health = value; }
+        }
+        /*
+        public Weapon Equipped_GS
+        {
+            get { return Equipped; }
+            set { Equipped = value; }
+        }
+        */
+        public int Age_GS
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+
+        public char Gender_GS
+        {
+            get { return Gender; }
+            set { Gender = value; }
+        }
+
+
     }
 }
--- NoelMowatt_CE02/NoelMowatt_CE02/Weapon.cs
+++ NoelMowatt_CE02/NoelMowatt_CE02/Weapon.cs
@@ -8,5 +8,29 @@ namespace NoelMowatt_CE02
 {
     class Weapon
     {
+        //--Class member fields
+        private int Attack;
+        private int RegenrateHealth;
+
+        //--Constructor
+        public Weapon(int attack, int regenerateHealth)
+        {
+            Attack = attack;
+            RegenrateHealth = regenerateHealth;
+        }
+
+        //---------Getters and Setters------------------//
+        public int Attack_GS
+        {
+            get { return Attack; }
+            set { Attack = value; }
+        }
+
+        public int RegenrateHealth_GS
+        {
+            get { return RegenrateHealth; }
+            set { RegenrateHealth = value; }
+        }
+        //-------------------------------------------------//
     }
 }
