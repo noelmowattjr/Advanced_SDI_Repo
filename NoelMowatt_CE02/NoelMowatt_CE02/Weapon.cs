ce8561c09392f498f46bf5bac8c8a20940642ab9
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-28 15:50:37 -0500

    Initial commit
    Initial commit - program setup!

---
+++ NoelMowatt_CE02/NoelMowatt_CE02/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoelMowatt_CE02
+{
+    class Weapon
+    {
+    }
+}
