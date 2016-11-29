d7f74609a7a51c34672041ec9ec852e908c0fe86
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-28 21:28:08 -0500

    UpperCase method
    Created a function that capitalizes the initial letter of a string

--- NoelMowatt_CE02/NoelMowatt_CE02/Program.cs
+++ NoelMowatt_CE02/NoelMowatt_CE02/Program.cs
@@ -8,26 +8,45 @@ namespace NoelMowatt_CE02
 {
     class Program
     {
+        static Character superHero;
+
         static void Main(string[] args)
         {
-            Character Superman = new Character(name:"Clark Kent",baseattack:100,health:95,age:30,gender:'M');
+            //--Prompt user for favorite hero
+            Console.Write("Choose one of the following as your favorite superhero\r\n1) Superman\r\n2) Batman\r\n3) Wonder Woman\r\n ");
+            string entryHero = Console.ReadLine();  //--Catch user's response
 
-
-            Console.Write("How strong is Superman? type a number and press enter: ");
-            string entry = Console.ReadLine();
-            int supesStrength;  //--converted string to be stored here
-
-            //--Validate user entered a number
-            while (!(int.TryParse(entry, out supesStrength)))
+            //------------------VALIDATION---------------------------//
+            while (entryHero.ToLower() != "superman" && entryHero.ToLower() != "batman" && entryHero.ToLower() != "wonder woman")
             {
-                Console.Write("Enter a whole number: ");
-                entry = Console.ReadLine();
+                Console.WriteLine("INVALID Entry!\r\nPlease choose \"1)Superman 2)Batman! 3)Wonder Woman)\" and press enter: ");
+                entryHero = Console.ReadLine();
             }
+            //------------------------------------------------------//
 
-            Weapon strength = new Weapon(attack: supesStrength, shield: true);
-            var a = strength.Attack_GS;
-            Console.WriteLine("Superman's strength is {0}", a);
+            string hero = CapFirstLetter(entryHero);     //--Conververt user's response to where first letter is upper case
+            
+            //--Give user feedback
+            Console.WriteLine("You chose {0} as your favorite superhero!", hero);
+            
+            
+        }
+        
+        //--convert only first letter of string to upper case
+        public static string CapFirstLetter(string convertThis)
+        {
+            //--If string is empty then nothing happens
+            if (convertThis == null)
+                return null;
 
+            //--If string length is longer than 1
+            if (convertThis.Length > 1)
+
+                //--then capitalize the the initial index of string and append the rest of the string back onto itself
+                return char.ToUpper(convertThis[0]) + convertThis.Substring(1);
+
+            //--the result I want =)
+            return convertThis.ToUpper();
         }
     }
 }
