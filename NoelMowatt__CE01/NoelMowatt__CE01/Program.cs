a93fa3fc1cc4e1829f1eb6c84cce8ee9156cf5b4
Noel Mowatt <NoelMowattJr@gmail.com>
2017-02-03 16:01:09 -0500

    Instantiated all three classes - rock, paper, scissors

--- NoelMowatt__CE01/NoelMowatt__CE01/Program.cs
+++ NoelMowatt__CE01/NoelMowatt__CE01/Program.cs
@@ -10,6 +10,35 @@ namespace NoelMowatt__CE01
     {
         static void Main(string[] args)
         {
+            //--Instantiation of Rock Class
+            Rock rock = new Rock(texture:"rough",roll:false,numOfSides:11);
+            string surface = rock.GS_texture;
+            bool canRoll = rock.GS_roll;
+            int numSides = rock.GS_numOfSides;
+
+            surface = "smooth";
+            canRoll = true;
+            numSides = 1;
+
+            //--Instantiation of Paper Class
+            Paper paper = new Paper(color: "white", numOfSheets: 1, isPrintingPaper: true);
+            string kolor = paper.GS_color;
+            int numSheets = paper.GS_numOfSheets;
+            bool printable = paper.GS_printablePaper;
+
+            kolor = "Night Blue";
+            numSheets = 28;
+            printable = true;
+
+            //--Instantiation of Scissors Class
+            Scissors scissors = new Scissors(isSharp: true, materialOfHandle: "Rubber", numOfBlades: 2);
+            bool sharp = scissors.GS_isSharp;
+            string handleMaterial = scissors.GS_materialOfhandle;
+            int numBlades = scissors.GS_numBlades;
+
+            sharp = true;
+            handleMaterial = "Recycled Rubber";
+            numBlades = 2;
         }
     }
 }
